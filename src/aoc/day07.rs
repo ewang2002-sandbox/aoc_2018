a3814e8216cd0076f1d86aa481f6bb296c6a4a10
Edward Wang <37031713+ewang20027@users.noreply.github.com>
2021-07-14 14:01:38 -0700

    Day 07 Part 2 - Might need to revisit to clean code up

--- src/aoc/day07.rs
+++ src/aoc/day07.rs
@@ -1,7 +1,7 @@
 use std::collections::HashSet;
 
 #[allow(dead_code)]
-pub fn execute(input: &Vec<String>) -> (String, i32) {
+pub fn execute(input: &Vec<String>) -> (String, usize) {
     let steps: Vec<_> = input.iter()
         .map(|x| {
             // Step C must be finished before step A can begin.
@@ -11,7 +11,31 @@ pub fn execute(input: &Vec<String>) -> (String, i32) {
         })
         .collect();
 
-    return (part1(&steps), part2());
+    // Potential candidates that we should look into.
+    let mut base_letters: HashSet<&String> = HashSet::new();
+
+    // Step 0: We're looking for a (req, for) pair such that we don't see req anywhere in the "for"
+    // member of any Step struct in the vector. That is, we're essentially doing:
+    //      all_for = {map instructions to its for_part member.}
+    //      all_req = {map instructions to its requirements member.}
+    //  =>  result = all_req \ all_for
+    // Then, we can add both the requirement and for_part members to the possible available set.
+    //
+    // The important thing to keep in mind is that, unlike the example, there are *multiple*
+    // pairs that can be the "first" character in the string.
+    //
+    // Don't remove any instructions yet as we haven't actually updated the final string with these
+    // values; we're only finding potential candidates to start with.
+    for instruction in steps.iter() {
+        if steps.iter().any(|y| y.for_part == instruction.requirement) {
+            continue;
+        }
+
+        base_letters.insert(&instruction.requirement);
+        base_letters.insert(&instruction.for_part);
+    }
+
+    return (part1(&steps, &base_letters), part2(&steps, &base_letters));
 }
 
 // https://adventofcode.com/2018/day/7
@@ -68,30 +92,11 @@ pub fn execute(input: &Vec<String>) -> (String, i32) {
 // In what order should the steps in your instructions be completed?
 
 #[allow(dead_code)]
-pub fn part1(ins: &Vec<Step>) -> String {
+pub fn part1(ins: &Vec<Step>, base_letters: &HashSet<&String>) -> String {
     let mut instructions: Vec<&Step> = ins.iter().collect();
     let mut finished = String::new();
-    let mut poss_avail: HashSet<&String> = HashSet::new();
-
-    // Step 1: We're looking for a (req, for) pair such that we don't see req anywhere in the "for"
-    // member of any Step struct in the vector. That is, we're essentially doing:
-    //      all_for = {map instructions to its for_part member.}
-    //      all_req = {map instructions to its requirements member.}
-    //  =>  result = all_req \ all_for
-    // Then, we can add both the requirement and for_part members to the possible available set.
-    //
-    // The important thing to keep in mind is that, unlike the example, there are *multiple*
-    // pairs that can be the "first" character in the string.
-    let mut indices_to_remove: Vec<usize> = vec![];
-    for (idx, instruction) in instructions.iter().enumerate() {
-        if instructions.iter().any(|y| y.for_part == instruction.requirement) {
-            continue;
-        }
-
-        indices_to_remove.push(idx);
-        poss_avail.insert(&instruction.requirement);
-        poss_avail.insert(&instruction.for_part);
-    }
+    // Clone this since we need the original hashset for part 2.
+    let mut poss_avail = base_letters.clone();
 
     // Go through the instructions...
     while !poss_avail.is_empty() {
@@ -180,12 +185,164 @@ pub fn part1(ins: &Vec<Step>) -> String {
 // complete all of the steps?
 
 #[allow(dead_code)]
-pub fn part2() -> i32 {
-    return -1;
+pub fn part2(ins: &Vec<Step>, base_letters: &HashSet<&String>) -> usize {
+    let mut instructions: Vec<&Step> = ins.iter().collect();
+    let mut finished = String::new();
+    let mut poss_avail: HashSet<&String> = base_letters.iter().map(|x| *x).collect();
+
+    let mut workers: [Worker; 5] = [
+        Worker { id: 1, curr_job: EMPTY_STR, time_left: 0 },
+        Worker { id: 2, curr_job: EMPTY_STR, time_left: 0 },
+        Worker { id: 3, curr_job: EMPTY_STR, time_left: 0 },
+        Worker { id: 4, curr_job: EMPTY_STR, time_left: 0 },
+        Worker { id: 5, curr_job: EMPTY_STR, time_left: 0 }
+    ];
+
+    let mut time_taken: usize = 0;
+    loop {
+        if poss_avail.is_empty() && workers.iter().all(|x| !x.has_assigned_job()) {
+            break;
+        }
+
+        // Get all instructions that we can work on.
+        let mut all_avail: Vec<&String> = vec![];
+        for &elem in &poss_avail {
+            if instructions.iter().any(|x| x.for_part == *elem) {
+                continue;
+            }
+
+            all_avail.push(elem);
+        }
+
+        // Sort them.
+        all_avail.sort_unstable();
+
+        // Then, allocate these jobs to the workers.
+        // TODO how to not use an indexed-for loop to set values?
+        // Seems like if you want to use a normal for each loop, an error will be brought up
+        // regarding immutability.
+        for i in 0..workers.len() {
+            if workers[i].has_assigned_job() {
+                continue;
+            }
+
+            if all_avail.len() == 0 {
+                break;
+            }
+
+            workers[i].set_job(all_avail[0]);
+            poss_avail.retain(|&item| item != all_avail[0]);
+            all_avail.remove(0);
+        }
+
+        // And then start working.
+        for i in 0..workers.len() {
+            if !workers[i].has_assigned_job() {
+                continue;
+            }
+
+            let is_done = workers[i].work();
+            if is_done {
+                let completed_job = workers[i].return_job_and_reset().unwrap();
+                assert_ne!(completed_job, EMPTY_STR);
+
+                // Remove any instructions that we've essentially done, and adds more instructions
+                // that we could possibly work on.
+                instructions.retain(|&item| {
+                    if completed_job == item.requirement {
+                        poss_avail.insert(&item.for_part);
+                        return false;
+                    }
+
+                    return true;
+                });
+
+                finished += completed_job.as_str();
+            }
+        }
+
+        time_taken += 1;
+    }
+
+    return time_taken;
 }
 
 #[derive(Clone, Debug)]
 pub struct Step {
     requirement: String,
-    for_part: String
+    for_part: String,
+}
+
+const EMPTY_STR: String = String::new();
+
+pub struct Worker {
+    #[allow(dead_code)]
+    id: i8,
+    curr_job: String,
+    time_left: usize,
+}
+
+impl Worker {
+    /// Sets this worker's job. If the worker already has a job, then this does not do anything.
+    ///
+    /// # Parameters
+    /// * `job_id` - The job ID.
+    ///
+    /// # Returns
+    /// `true` if the job was set; `false` otherwise.
+    fn set_job(&mut self, job_id: &String) -> bool {
+        if self.time_left > 0 {
+            return false;
+        }
+
+        self.curr_job = job_id.to_string();
+        self.time_left = 60 + (job_id.as_bytes()[0] as usize) - 64;
+        return true;
+    }
+
+    /// Checks if this worker has an assigned job. This does not check if the time remaining is 0,
+    /// only that this worker has a job assigned to it.
+    ///
+    /// # Returns
+    /// `true` if this worker has a job, `false` otherwise.
+    fn has_assigned_job(&self) -> bool {
+        return self.curr_job != EMPTY_STR;
+    }
+
+    /// Makes the worker work for 1 second. All this does is decrements the `time_left` member by 1
+    /// (essentially simulating 1 second of work).
+    ///
+    /// # Returns
+    /// `true` if this worker completed its job, `false` if the worker still needs to work.
+    fn work(&mut self) -> bool {
+        if self.curr_job == EMPTY_STR {
+            return false;
+        }
+
+        self.time_left -= 1;
+        return self.time_left == 0;
+    }
+
+    /// Returns the job ID and resets it.
+    ///
+    /// # Returns
+    /// A string representing the job ID if the worker is not working. If the worker is working,
+    /// then this returns nothing.
+    fn return_job_and_reset(&mut self) -> Option<String> {
+        if self.is_working() {
+            return None;
+        }
+
+        let str_to_return = String::from(&self.curr_job);
+        self.curr_job = EMPTY_STR;
+        return Some(str_to_return);
+    }
+
+    /// Checks if the worker still needs to work on the job.
+    ///
+    /// # Returns
+    /// `true` if the worker is still working, `false` otherwise.
+    fn is_working(&self) -> bool {
+        return self.time_left > 0;
+    }
 }
