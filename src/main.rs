cf4fe89a66147c94060d47a7446425e1fcddfae0
Edward Wang <37031713+ewang20027@users.noreply.github.com>
2021-11-24 11:52:05 -0800

    Day 11 part 1, started 2

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,13 @@ mod aoc;
 mod helpers;
 
 fn main() {
-    let input_file = io::file_read_all_lines("input/day10.txt");
+    let input_file = io::file_read_all_lines("input/day11.txt");
 
     // Execution begins
     let start = Instant::now();
 
     // Get both parts
-    let (part1_sol, part2_sol) = day10::execute(&input_file);
+    let (part1_sol, part2_sol) = day11::execute(&input_file);
     println!("Solution to Part 1: {}", part1_sol);
     println!("Solution to Part 2: {}", part2_sol);
 
