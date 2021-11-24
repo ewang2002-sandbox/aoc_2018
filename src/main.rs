19781324d2fcda05f5e2e1bd7558891d52e32d09
Edward Wang <37031713+ewang20027@users.noreply.github.com>
2021-11-24 01:49:57 -0800

    Day 10 Part 1 & 2 - Bruteforce complete!

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,18 @@
 use std::time::Instant;
 use helpers::io;
+use aoc::*;
 
 mod aoc;
 mod helpers;
 
 fn main() {
-    let input_file = io::file_read_all_lines("input/day09.txt");
+    let input_file = io::file_read_all_lines("input/day10.txt");
 
     // Execution begins
     let start = Instant::now();
 
-    use aoc::day09 as aoc_problem;
-
     // Get both parts
-    let (part1_sol, part2_sol) = aoc_problem::execute(&input_file);
+    let (part1_sol, part2_sol) = day10::execute(&input_file);
     println!("Solution to Part 1: {}", part1_sol);
     println!("Solution to Part 2: {}", part2_sol);
 
