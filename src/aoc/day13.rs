ca5cfb8fbda0c9ca3a0c4583ad900f32f2cebf69
Edward Wang <37031713+ewang20027@users.noreply.github.com>
2021-11-26 09:07:24 -0800

    Finished Day 13 part 1

--- src/aoc/day13.rs
+++ src/aoc/day13.rs
@@ -1,3 +1,4 @@
+use std::cmp::Ordering;
 use std::collections::{HashSet};
 use std::fmt::{Display, Formatter, Write};
 
@@ -52,57 +53,13 @@ pub fn part1(track_system: &Vec<Vec<Track>>, active_carts: &Vec<Cart>) -> String
 
     let mut conflicting_coords: Option<Coord> = None;
     while conflicting_coords.is_none() {
-        print_track(track_system, &all_active_carts);
         let mut next_active_carts: Vec<Cart> = vec![];
         let mut next_active_coords: HashSet<Coord> = HashSet::new();
-        for (cart, dir, x, y) in all_active_carts {
+        for cart in all_active_carts {
+            let (_, dir, x, y) = cart;
             all_active_coords.remove(&(x, y));
             // determine the next track
-            let next_track = match cart {
-                CART_DOWN => &track_system[y + 1][x],
-                CART_UP => &track_system[y - 1][x],
-                CART_RIGHT => &track_system[y][x + 1],
-                CART_LEFT => &track_system[y][x - 1],
-                _ => panic!("invalid cart given: {}", cart)
-            };
-
-            // determine the next cart
-            let next_cart = match next_track.raw_track_type {
-                STRAIGHT_VERT | STRAIGHT_HORIZ => cart,
-                CURVE_SLASH => match cart {
-                    '^' => '>',
-                    '<' => 'v',
-                    _ => panic!("invalid cart {} given with dir {}", cart, CURVE_SLASH)
-                },
-                CURVE_BACKSLASH => match cart {
-                    '>' => 'v',
-                    '^' => '<',
-                    _ => panic!("invalid cart {} given with dir {}", cart, CURVE_SLASH)
-                },
-                INTERSECTION => match dir {
-                    // Left
-                    0 => match cart {
-                        'v' => '>',
-                        '>' => '^',
-                        '^' => '<',
-                        '<' => 'v',
-                        _ => panic!("invalid cart {}", cart)
-                    },
-                    // Straight
-                    1 => cart,
-                    // Right
-                    2 => match cart {
-                        'v' => '<',
-                        '>' => 'v',
-                        '^' => '>',
-                        '<' => '^',
-                        _ => panic!("invalid cart {}", cart)
-                    },
-                    _ => panic!("invalid cart {} given with dir", cart)
-                },
-                _ => panic!("invalid track {}", next_track.raw_track_type)
-            };
-
+            let (next_cart, next_track) = get_next_cart_track(cart, track_system);
             next_active_carts.push((
                 next_cart,
                 if next_track.raw_track_type == INTERSECTION { (dir + 1) % 3 } else { dir },
@@ -121,17 +78,23 @@ pub fn part1(track_system: &Vec<Vec<Track>>, active_carts: &Vec<Cart>) -> String
 
         all_active_carts = next_active_carts;
         all_active_coords = next_active_coords;
+        sort_carts_by_row_col(&mut all_active_carts);
     }
 
     return format!("{},{}", conflicting_coords.unwrap().0, conflicting_coords.unwrap().1);
 }
-
 
 pub fn part2(input: &Vec<String>) -> i64 {
     return input.len() as i64;
 }
 
 
+/// Prints the track out. Useful for debugging.
+///
+/// # Parameters
+/// - `track`: The track.
+/// - `carts`: The carts.
+#[allow(dead_code)]
 fn print_track(track: &Vec<Vec<Track>>, carts: &Vec<Cart>) -> () {
     for y in 0..track.len() {
         for x in 0..track[y].len() {
@@ -144,6 +107,8 @@ fn print_track(track: &Vec<Vec<Track>>, carts: &Vec<Cart>) -> () {
 
         println!();
     }
+
+    println!();
 }
 
 pub struct Track {
@@ -153,8 +118,20 @@ pub struct Track {
 }
 
 impl Track {
+    /// Creates a new `Track` with the specified character and position.
+    ///
+    /// # Parameters
+    /// - `track_char`: The track character. Should be one of < > ^ v / \ | - +.
+    /// - `x_pos`: The `x`-position of this track.
+    /// - `y_pos`: The `y`-position of this track.
     pub fn new(track_char: char, x_pos: usize, y_pos: usize) -> Self {
-        return Track { x_pos, y_pos, raw_track_type: track_char };
+        let track_to_use: char = match track_char {
+            CART_UP | CART_DOWN => STRAIGHT_VERT,
+            CART_RIGHT | CART_LEFT => STRAIGHT_HORIZ,
+            rest => rest
+        };
+
+        return Track { x_pos, y_pos, raw_track_type: track_to_use };
     }
 }
 
@@ -162,4 +139,97 @@ impl Display for Track {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         return f.write_char(self.raw_track_type);
     }
+}
+
+/// Sorts all carts by row/column order. This will first sort every cart by x-coordinate, and then
+/// sort every cart by y-coordinate.
+///
+/// Remember: Carts on the top row move first (acting from left to right), then carts on the second
+/// row move (again from left to right), then carts on the third row, and so on.
+///
+/// # Parameters
+/// - `all_active_carts`: All active carts.
+fn sort_carts_by_row_col(all_active_carts: &mut Vec<Cart>) -> () {
+    all_active_carts.sort_by(|cart_a, cart_b| {
+        let (_, _, ax, ay) = *cart_a;
+        let (_, _, bx, by) = *cart_b;
+        if ax < bx {
+            return Ordering::Less;
+        }
+        else if ax > bx {
+            return Ordering::Greater;
+        }
+
+        if ay < by {
+            return Ordering::Less;
+        }
+        else if ay > by {
+            return Ordering::Greater;
+        }
+
+        return Ordering::Equal;
+    })
+}
+
+/// Gets the next cart and track.
+///
+/// # Parameters
+/// - `cart_tuple`: The cart itself.
+/// - `track_system`: The track system.
+///
+/// # Returns
+/// - A tuple containing the next cart character and the next track. You will need to manually
+/// adjust any properties (like direction count, if applicable).
+fn get_next_cart_track(cart_tuple: Cart, track_system: &Vec<Vec<Track>>) -> (char, &Track) {
+    let (cart, dir, x, y) = cart_tuple;
+    let next_track = match cart {
+        CART_DOWN => &track_system[y + 1][x],
+        CART_UP => &track_system[y - 1][x],
+        CART_RIGHT => &track_system[y][x + 1],
+        CART_LEFT => &track_system[y][x - 1],
+        _ => panic!("invalid cart given: {}", cart)
+    };
+
+    // determine the next cart
+    let next_cart = match next_track.raw_track_type {
+        STRAIGHT_VERT | STRAIGHT_HORIZ => cart,
+        CURVE_SLASH => match cart {
+            '^' => '>',
+            '<' => 'v',
+            '>' => '^',
+            'v' => '<',
+            _ => panic!("invalid cart {} given with dir {}", cart, CURVE_SLASH)
+        },
+        CURVE_BACKSLASH => match cart {
+            '>' => 'v',
+            '^' => '<',
+            '<' => '^',
+            'v' => '>',
+            _ => panic!("invalid cart {} given with dir {}", cart, CURVE_SLASH)
+        },
+        INTERSECTION => match dir {
+            // Left
+            0 => match cart {
+                'v' => '>',
+                '>' => '^',
+                '^' => '<',
+                '<' => 'v',
+                _ => panic!("invalid cart {}", cart)
+            },
+            // Straight
+            1 => cart,
+            // Right
+            2 => match cart {
+                'v' => '<',
+                '>' => 'v',
+                '^' => '>',
+                '<' => '^',
+                _ => panic!("invalid cart {}", cart)
+            },
+            _ => panic!("invalid cart {} given with dir", cart)
+        },
+        _ => panic!("invalid track {}", next_track.raw_track_type)
+    };
+
+    return (next_cart, next_track);
 }
